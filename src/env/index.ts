bfb6adf7fe553b68a62229be24aa985831cb5ac2
Dikezeko Oliveira <dikezeko.oliveira@foursys.com.br>
2024-09-16 17:26:52 -0300

    env port

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,7 +4,7 @@ import { z } from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(["dev", "production", "test"]).default("dev"),
   DATABASE_URL: z.string(),
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(80),
 });
 
 const _env = envSchema.safeParse(process.env);
