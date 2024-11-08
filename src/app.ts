f0a3ef0b5c1d87de21f3508179a414b5d81ed09d
Dikezeko Oliveira <dikezekodagraca04@gmail.com>
2024-11-08 00:49:03 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,48 +1,48 @@
-import fastify from 'fastify'
-import cors from '@fastify/cors'
-import { ZodError } from 'zod'
-import 'module-alias/register'
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { ZodError } from "zod";
+import "module-alias/register";
 
 import "./services/jobs/emails/send-scores";
-import { env } from './env'
-import { questionsRoutes } from './http/controllers/questions/routes'
-import { companiesRoutes } from './http/controllers/companies/routes'
-import { scoresRoutes } from './http/controllers/scores/routes'
-import { emailsRoutes } from './http/controllers/emails/routes'
+import { env } from "./env";
+import { questionsRoutes } from "./http/controllers/questions/routes";
+import { companiesRoutes } from "./http/controllers/companies/routes";
+import { scoresRoutes } from "./http/controllers/scores/routes";
+import { emailsRoutes } from "./http/controllers/emails/routes";
 
-export const app = fastify()
+export const app = fastify();
 
 const allowedDomains = [
-  'https://sicredi.firgun.com.br/',
-  'https://www.sicredi.firgun.com.br/',
-]
+  "https://sicredi.firgun.com.br",
+  "https://www.sicredi.firgun.com.b",
+];
 
 app.register(cors, {
   origin: function (origin, callback) {
     // bypass the requests with no origin (like curl requests, mobile apps, etc )
-    if (!origin) return callback(null, true)
+    if (!origin) return callback(null, true);
     if (allowedDomains.indexOf(origin) === -1) {
-      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`
-      return callback(new Error(msg), false)
+      const msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+      return callback(new Error(msg), false);
     }
-    return callback(null, true)
+    return callback(null, true);
   },
-})
-app.register(questionsRoutes)
-app.register(companiesRoutes)
-app.register(scoresRoutes)
-app.register(emailsRoutes)
+});
+app.register(questionsRoutes);
+app.register(companiesRoutes);
+app.register(scoresRoutes);
+app.register(emailsRoutes);
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
     return reply
       .status(400)
-      .send({ message: 'Validation error.', issues: error.format() })
+      .send({ message: "Validation error.", issues: error.format() });
   }
 
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   }
 
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
+  return reply.status(500).send({ message: "Internal server error." });
+});
